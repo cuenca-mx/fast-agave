b4a52cda95cb9033261d390d283b40d0cb81ba41
Felipe López <flh.1989@gmail.com>
2021-09-20 17:29:20 -0500

    fix

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,12 +13,6 @@ def test_cuenca_error_handler(client: TestClient) -> None:
     assert resp.json() == dict(error='you are not lucky enough!', code=101)
 
 
-def test_cuenca_error_handler(client: TestClient) -> None:
-    resp = client.get('/raise_cuenca_errors')
-    assert resp.status_code == 401
-    assert resp.json() == dict(error='you are not lucky enough!', code=101)
-
-
 def test_fast_agave_error_handler(client: TestClient) -> None:
     resp = client.get('/raise_fast_agave_errors')
     assert resp.status_code == 401
