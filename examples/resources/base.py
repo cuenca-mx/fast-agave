fdc6266e045c89c8b84e349371f746f79c49ef2a
Felipe López <flh.1989@gmail.com>
2021-09-20 17:26:08 -0500

    add exception handlers

--- examples/resources/base.py
+++ examples/resources/base.py
@@ -1,6 +1,9 @@
-from typing import Dict
+from typing import Dict, NoReturn
+
+from cuenca_validations.errors import WrongCredsError
 
 from fast_agave.blueprints import RestApiBlueprint
+from fast_agave.exc import UnauthorizedError
 
 app = RestApiBlueprint()
 
@@ -8,3 +11,13 @@ app = RestApiBlueprint()
 @app.get('/healthy_auth')
 def health_auth_check() -> Dict:
     return dict(greeting="I'm authenticated and healthy !!!")
+
+
+@app.get('/raise_cuenca_errors')
+def raise_cuenca_errors() -> NoReturn:
+    raise WrongCredsError('you are not lucky enough!')
+
+
+@app.get('/raise_fast_agave_errors')
+def raise_fast_agave_errors() -> NoReturn:
+    raise UnauthorizedError('nice try!')
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,3 +5,21 @@ def test_iam_healthy(client: TestClient) -> None:
     resp = client.get('/')
     assert resp.status_code == 200
     assert resp.json() == dict(greeting="I'm healthy!!!")
+
+
+def test_cuenca_error_handler(client: TestClient) -> None:
+    resp = client.get('/raise_cuenca_errors')
+    assert resp.status_code == 401
+    assert resp.json() == dict(error='you are not lucky enough!', code=101)
+
+
+def test_cuenca_error_handler(client: TestClient) -> None:
+    resp = client.get('/raise_cuenca_errors')
+    assert resp.status_code == 401
+    assert resp.json() == dict(error='you are not lucky enough!', code=101)
+
+
+def test_fast_agave_error_handler(client: TestClient) -> None:
+    resp = client.get('/raise_fast_agave_errors')
+    assert resp.status_code == 401
+    assert resp.json() == dict(error='nice try!')
