e2521123debb6c20537383c67a178ed32c35f07f
Felipe López <flh.1989@gmail.com>
2021-09-20 18:13:14 -0500

    rename handler

--- examples/app.py
+++ examples/app.py
@@ -3,11 +3,10 @@ from typing import Dict
 from cuenca_validations.errors import CuencaError
 from mongoengine import connect
 from fastapi import FastAPI
-from starlette.middleware import Middleware
 
 from fast_agave.exc import FastAgaveError
 from fast_agave.exception_handlers import (
-    fast_agave_exceptions_handler,
+    fast_agave_errors_handler,
     cuenca_errors_handler,
 )
 from .resources import app as resources
@@ -15,9 +14,12 @@ from .middlewares import AuthedMiddleware
 
 
 connect(host='mongomock://localhost:27017/db')
-app = FastAPI(middleware=[Middleware(AuthedMiddleware)])
+app = FastAPI(title='example')
 app.include_router(resources)
-app.add_exception_handler(FastAgaveError, fast_agave_exceptions_handler)
+
+app.add_middleware(AuthedMiddleware)
+
+app.add_exception_handler(FastAgaveError, fast_agave_errors_handler)
 app.add_exception_handler(CuencaError, cuenca_errors_handler)
 
 
