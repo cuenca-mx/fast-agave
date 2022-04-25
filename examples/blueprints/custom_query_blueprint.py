624da2be642a99e6a7d1291f81340b0c4d372862
pachCode <arturo@cuenca.com>
2022-04-25 14:58:45 -0500

    change example

--- examples/blueprints/custom_query_blueprint.py
+++ examples/blueprints/custom_query_blueprint.py
@@ -4,17 +4,17 @@ from fast_agave.blueprints import RestApiBlueprint
 from starlette_context import context
 
 
-class ArteriaBlueprint(RestApiBlueprint):
+class CustomQueryBlueprint(RestApiBlueprint):
     @property
-    def wallet(self) -> str:
-        return context['wallet']
+    def custom(self) -> str:
+        return context['custom']
 
-    def wallet_filter_required(self) -> bool:
-        return context.get('wallet_filter_required')
+    def property_filter_required(self) -> bool:
+        return context.get('custom_filter_required')
 
     def custom_filter_required(self, query_params: Any, model: any) -> None:
-        if self.wallet_filter_required() and hasattr(model, 'wallet'):
-            query_params.wallet = self.wallet
+        if self.property_filter_required() and hasattr(model, 'custom'):
+            query_params.custom = self.custom
 
     def user_id_filter_required(self) -> bool:
         return False
