76d4a85779db07792bf7408e85f9771611c2fb48
pachCode <arturo@cuenca.com>
2022-04-25 13:48:31 -0500

    format

--- examples/blueprints/arteria_blueprint.py
+++ examples/blueprints/arteria_blueprint.py
@@ -8,14 +8,12 @@ class ArteriaBlueprint(RestApiBlueprint):
     @property
     def wallet(self) -> str:
         return context['wallet']
-    
+
     def wallet_filter_required(self) -> bool:
         return context.get('wallet_filter_required')
 
     def custom_filter_required(self, query_params: Any, model: any) -> None:
-        if self.wallet_filter_required() and hasattr(
-            model, 'wallet'
-        ):
+        if self.wallet_filter_required() and hasattr(model, 'wallet'):
             query_params.wallet = self.wallet
 
     def user_id_filter_required(self) -> bool:
