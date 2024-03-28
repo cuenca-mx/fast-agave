970e31cb47038001da52dded251767e70b5e8c3d
rogeliolopez <rogelio.lpz94@gmail.com>
2024-03-27 18:49:18 -0600

    bump dependencies

--- examples/resources/users.py
+++ examples/resources/users.py
@@ -1,9 +1,12 @@
+from typing import cast
+
 from fast_agave.filters import generic_query
 from ..models import User as UserModel
 from ..validators import UserQuery, UserUpdateRequest
 from .base import app
 from fastapi.responses import JSONResponse as Response
 from fastapi import Request
+from starlette.datastructures import Address
 
 
 @app.resource('/users')
@@ -18,6 +21,6 @@ class User:
         user: UserModel, request: UserUpdateRequest, api_request: Request
     ) -> Response:
         user.name = request.name
-        user.ip = api_request.client.host
+        user.ip = cast(Address, api_request.client).host
         await user.async_save()
         return Response(content=user.to_dict(), status_code=200)
